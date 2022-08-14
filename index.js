94a9b4f0145f51642427a07a5ab195f1c5e246a2
Andrew Darr <kildar07@gmail.com>
2022-08-14 14:29:23 -0500

    commit tree 639ed39066123aa4338eeacc24d3b5ca4e72461c

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random() * 23 ) + 1;
+// let mod = % 4
+let num3 = 25
+let num4 = 7
+let mod = num3 % num4
+let max = Math.max(10, 20);
 
-// Write your code here
+
+
+
+
+
+
+
+
+
+
